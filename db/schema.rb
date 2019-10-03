0eb97af4ca17c0cff1baf0772330982b05fc69fc
jlanp3 <jlanp3@gmail.com>
2019-10-03 01:15:24 -0300

    added confirmation email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_023247) do
+ActiveRecord::Schema.define(version: 2019_10_03_035659) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2019_10_03_023247) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
